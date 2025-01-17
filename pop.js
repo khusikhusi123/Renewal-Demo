9ab4a02bd67265f8f8840f25a517cc8d23a49127
Aditya kumar <83528896+Adityakr3@users.noreply.github.com>
2025-01-17 10:25:41 +0530

    add pops and media

--- pop.js
+++ pop.js
@@ -6,11 +6,32 @@ const cancelBtn = document.getElementById('cancelBtn');
 const openBtn = document.getElementById('open-terminal-btn');
 const closeBtn = document.getElementById('close-terminal-btn');
 const terminalPopup = document.getElementById('terminal-popup');
+const closeAlert = document.querySelector('.closeAlert');
 const dropbtn = document.querySelector('.dropbtn');
 const closeTerminalBtn = document.querySelector('#close-terminal-btn');
 const remove = document.querySelector('#remove');
 const addbtn = document.querySelector('#add');
+const alert = document.querySelector('.alertContaint');
+const alert1 = document.querySelector('.alertContaint1');
+const alert2 = document.querySelector('.alertContaint2');
+const ok1 = document.querySelector('.ok1');
+const ok2 = document.querySelector('.ok2');
+const ok3 = document.querySelector('.ok3');
 
+ok1.addEventListener('click',()=>{
+    alert.style.display = 'none';
+    alert1.style.display = 'block';
+    ok2.classList.add('blinking-text');
+})
+ok2.addEventListener('click',()=>{
+    alert1.style.display = 'none';
+    alert2.style.display = 'block';
+    ok3.classList.add('blinking-text');
+})
+ok3.addEventListener('click',()=>{
+    alert2.style.display = 'none';
+    addbtn.classList.add('blinking-text');
+})
 openPopupBtn.addEventListener('click', () => {
     popup.style.display = 'block';
     remove.classList.remove('blinking-text');
@@ -23,13 +44,17 @@ closePopup.addEventListener('click', () => {
 
 cancelBtn.addEventListener('click', () => {
     popup.style.display = 'none';
+    console.log("dfghjkl");
+    
 });
 
 openBtn.addEventListener('click', () => {
     // Handle form submission logic here
     terminalPopup.style.display = 'flex';
+    ok1.classList.add('blinking-text');
     popup.style.display = 'none';
     openPopupBtn.classList.remove('blinking-text');
+    closeTerminal()
 
 });
 
@@ -41,7 +66,20 @@ window.onclick = function(event) {
 };
 
 closeBtn.addEventListener('click', () => {
-  terminalPopup.style.display = 'none';
-  addbtn.classList.add('blinking-text');
+    terminalPopup.style.display = 'none';
+    addbtn.classList.add('blinking-text');
 });
+
+function closeTerminal(){
+    setTimeout(() => {
+        terminalPopup.style.display = 'none';
+        console.log('dfgfdgdfgdfg');
+        alert.style.display = 'block';
+    }, 2555); 
+}
+
+closeAlert.addEventListener('click', () => {
+  alert.style.display = 'none';
+})
+
 
